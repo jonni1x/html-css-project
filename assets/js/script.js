8560372eaf855f9c310919805f451840706eaa7d
jonni1x <jonzylfiu5@gmail.com>
2023-10-05 06:07:56 -0700

    changed main file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,20 @@
 const sectionHero = document.querySelector("section.hero");
 const dots = document.querySelectorAll('.page-numbers span');
+const burgerMenu = document.querySelector('.burger-menu')
+const navLinks = document.querySelector('nav .links');
 
-console.log(sectionHero, dots)
+burgerMenu.addEventListener('click', () => {
+    burgerMenu.classList.toggle('active');
+    navLinks.classList.toggle('active')
+})
 
 const images = [
-    '/assets/images/main-bg1.webp',
-    '/assets/images/main-bg2.webp',
-    '/assets/images/main-bg3.webp'
+    './assets/images/main-bg1.webp',
+    './assets/images/main-bg2.webp',
+    './assets/images/main-bg3.webp'
 ];
 
+sectionHero.style.background = `url(${images[0]}) no-repeat`;
 
 dots.forEach((dot, i) => {
     dot.addEventListener('click', () => {
@@ -24,14 +30,31 @@ dots.forEach((dot, i) => {
     });
 })
 
-let i = 0;
 setInterval(() => {
-    ++i
-    if(i > images.length - 1) {
-        i = 0
-    }
+    let i = 0;
+    i++
+    if(i > images.length - 1) i = 0
     
     sectionHero.style.background = `url(${images[i]}) no-repeat`;
     sectionHero.style.backgroundSize = 'cover';
     sectionHero.style.backgroundAttachment = 'fixed';
+
+    dots.forEach(dot => {
+        dot.classList.remove('active')
+    });
+
+    dots[i].classList.add('active');
 }, 2000)
+
+
+const ipAddressUrl = 'https://api.ipify.org?format=json';
+
+fetch(ipAddressUrl)
+  .then((response) => response.json())
+  .then((data) => {
+    const ipAddress = data.ip;
+    console.log('User IP Address:', ipAddress);
+  })
+  .catch((error) => {
+    console.error('Error fetching IP address:', error);
+  });
---
+++ main.php
@@ -0,0 +1,422 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="author" content="Jon Zylfiu">
+    <meta name="keywords" content="HTML, CSS">
+    <meta name="description" content="HTML, CSS responsive website">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pexman</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet">
+    <script src="https://code.iconify.design/2/2.2.1/iconify.min.js"></script>
+    <script src="https://kit.fontawesome.com/fbf88eb110.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="logo">
+                <h4>Pexman</h4>
+                <span>creative agency</span>
+            </div>
+            <ul class="links">
+                <li><a href="#" class="active">home</a></li>
+                <li><a href="#">about</a></li>
+                <li><a href="#">works</a></li>
+                <li><a href="#">blog</a></li>
+                <li><a href="#">contact</a></li>
+            </ul>
+    
+            <div class="burger-menu">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <section class="hero">
+            <aside>
+                <div class="page-numbers">
+                    <span class="active">01.</span>
+                    <span>02.</span>
+                    <span>03.</span>
+                </div>
+                <div class="description">
+                    <h1>
+                        Build Stunning Websites Design
+                    </h1>
+                    <p>
+                        A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. 
+                    </p>
+                    <button>
+                        view portfolio <span class="iconify" data-icon="bi:arrow-right"></span>
+                    </button>
+                </div>
+            </aside>
+        </section>
+
+        <section class="about">
+            <div class="about-description">
+                <div>
+                    <h2>
+                        <span>
+                            WELCOME PEXMAN
+                        </span>
+                    
+                        We Are Creative Agency That Create Beautiful Websites
+                    </h2>
+                    <p>
+                        Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.
+                    </p>
+                    <p>
+                        Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.
+                    </p>
+                    <button>start a project <span class="iconify" data-icon="bi:arrow-right"></span></button>
+                </div> 
+            </div>
+                
+            <div class="image">
+                <img src="./assets/images/about-img.png" alt="">
+            </div>
+    
+            <div class="achievements">
+                <div class="rating">
+                    <h4>
+                        10 
+                        <span>K+</span>
+                    </h4>
+                    <h5>
+                        ACHIEVEMENTS
+                    </h5>
+               </div>
+               <div class="rating">
+                    <h4>
+                        21
+                        <span>K+</span>
+                    </h4>
+                    <h5>
+                        PROJECT COMPLETED
+                    </h5>
+                    
+                </div>
+                <div class="rating">
+                    <h4>
+                        27 
+                    </h4>
+                    <h5>
+                        YEARS OF EXPERIENCED
+                    </h5>
+                </div>
+                <div class="rating">
+                    <h4>
+                        30
+                        <span>K+</span>
+                    </h4>
+                    <h5>
+                        HAPPY CUSTOMERS
+                    </h5>
+                </div>
+            </div>
+        </section>
+
+        <section class="offers">
+            <div class="image">
+                <img src="./assets/images/logo.png" alt="">
+            </div>
+            <div class="offer-description">
+                
+                    <h2>
+                        <span>what we offer</span>
+                        What We Offer
+                    </h2>
+                    <p class="description-p">
+                        Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.
+                    </p>
+                    <ul>
+                        <li>
+                            <div class="circle">
+                                1
+                            </div>
+                            <p>
+                                <span>Research</span>
+                                <span>A small river named Duden flows by their place.</span>
+                            </p>
+                        </li>
+                        <li>
+                            <div class="circle">
+                                2
+                            </div>
+                            <p>
+                                <span>Design</span>
+                                <span>A small river named Duden flows by their place.</span>
+                            </p>
+                        </li>
+                        <li>
+                            <div class="circle">
+                                3
+                            </div>
+                            <p>
+                                <span>Development</span>
+                                <span>A small river named Duden flows by their place.</span>
+                            </p>
+                        </li>
+                        <li>
+                            <div class="circle">
+                                4
+                            </div>
+                            <p>
+                                <span>Testing</span>
+                                <span>A small river named Duden flows by their place.</span>
+                            </p>
+                    </li>
+                </ul>
+                <button>start a project <span class="iconify" data-icon="bi:arrow-right"></span></button>
+            
+        </div>
+    </section>
+
+    <section class="cards">
+        <div class="card">
+            <img src="./assets/images/pc.png" />
+            <h2>Web Design</h2>
+            <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+        </div>
+        <div class="card">
+            <img src="./assets/images/camera.png" />
+            <h2>Photography</h2>
+            <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+        </div>
+        <div class="card">
+            <img src="./assets/images/sc-pc.png" />
+            <h2>Marketing</h2>
+            <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+        </div>
+        <div class="card">
+            <img src="./assets/images/pen.png" />
+            <h2>Graphic Design</h2>
+            <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+        </div>
+    </section>
+
+    <section class="work">
+        <h3>OUR PORTFOLIO</h3>
+        <div class="filter-projects">
+            <h2>Our Stunning Works</h2>
+            <ul>
+                <li><a href="#" class="active">ALL</a></li>
+                <li><a href="#">BRANDING</a></li>
+                <li><a href="#">WEB DESIGN</a></li>
+                <li><a href="#">ILLUSTRATION</a></li>
+                <li><a href="#">APPLICATION</a></li>
+            </ul>
+        </div>
+            <div class="projects">
+                <div class="image">
+                    <img src="./assets/images/project-1.png" />
+                    <a href="#">Hark Website</a>
+                    <span>web design,branding</span>
+                </div>
+                <div class="image">
+                    <img src="./assets/images/project-2.png" />
+                    <a href="#">Hark Website</a>
+                    <span>web design,branding</span>
+                </div>
+                <div class="image">
+                    <img src="./assets/images/project-3.png" />
+                    <a href="#">Hark Website</a>
+                    <span>web design,branding</span>
+                </div>
+                <div class="image">
+                    <img src="./assets/images/project-4.png" />
+                    <a href="#">Hark Website</a>
+                    <span>web design,branding</span>
+                </div>
+                <div class="image">
+                    <img src="./assets/images/project-5.png" />
+                    <a href="#">Hark Website</a>
+                    <span>web design,branding</span>
+                </div>
+                <div class="image">
+                    <img src="./assets/images/project-6.png" />
+                    <a href="#">Hark Website</a>
+                    <span>web design,branding</span>
+                </div>
+                <div class="image">
+                    <img src="./assets/images/project-7.png" />
+                    <a href="#">Hark Website</a>
+                    <span>web design,branding</span>
+                </div>
+                <div class="image">
+                    <img src="./assets/images/project-8.png" />
+                    <a href="#">Hark Website</a>
+                    <span>web design,branding</span>
+                </div>
+            </div>
+    </section>
+
+    <section class="clients">
+        <span>TESTIMONIAL</span>
+        <h2>What Are Clients Says</h2>
+        <div class="client">
+            <img src="./assets/images/client-1.png" alt="" />
+            <div class="rating">
+                <span class="iconify" data-icon="ant-design:star-filled" style="color: #d9bf77;"></span>
+                <span class="iconify" data-icon="ant-design:star-filled" style="color: #d9bf77;"></span>
+                <span class="iconify" data-icon="ant-design:star-filled" style="color: #d9bf77;"></span>
+                <span class="iconify" data-icon="ant-design:star-filled" style="color: #d9bf77;"></span>
+                <span class="iconify" data-icon="ant-design:star-filled" style="color: #d9bf77;"></span>
+            </div>
+            <p> Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+            <h4>Roger Scott</h4>
+            <span>Chief Executive Officer @pexman</span>
+        </div>
+        <div class="dots">
+            <span class="dot"></span>
+            <span class="dot active"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+        </div>
+    </section>
+
+    <section class="blog">
+        <span>READ LATEST NEWS</span>
+        <h2>Our Latest Blog</h2>
+
+        <div class="articles">
+            <div class="article">
+                <img src="./assets/images/article-1.png" alt="" />
+                <div>
+                    <span class="iconify" data-icon="clarity:date-line" style="color: #999;"></span>
+                    OCT. 27, 2020
+                </div>
+                <h4>
+                    Far far away, behind the word mountains
+                </h4>
+                <p>
+                    Far far away, behind the word mountains, far from the countries.
+                </p>
+            </div>
+            <div class="article">
+                <img src="./assets/images/article-2.png" alt="" />
+                <div>
+                    <span class="iconify" data-icon="clarity:date-line" style="color: #999;"></span>
+                    OCT. 27, 2020
+                </div>
+                <h4>
+                    Far far away, behind the word mountains
+                </h4>
+                <p>
+                    Far far away, behind the word mountains, far from the countries.
+                </p>
+            </div>
+            <div class="article">
+                <img src="./assets/images/article-3.png" alt="" />
+                <div>
+                    <span class="iconify" data-icon="clarity:date-line" style="color: #999;"></span>
+                    OCT. 27, 2020
+                </div>
+                <h4>
+                    Far far away, behind the word mountains
+                </h4>
+                <p>
+                    Far far away, behind the word mountains, far from the countries.
+                </p>
+            </div>
+            <div class="article">
+                <img src="./assets/images/article-4.png" alt="" />
+                <div>
+                    <span class="iconify" data-icon="clarity:date-line" style="color: #999;"></span>
+                    OCT. 27, 2020
+                </div>
+                <h4>
+                    Far far away, behind the word mountains
+                </h4>
+                <p>
+                    Far far away, behind the word mountains, far from the countries.
+                </p>
+            </div>
+        </div>
+    </section>
+    </main>
+
+    <footer>
+        <div class="pexman">
+            <h2>Pexman</h2>
+            <p>
+                Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.
+            </p>
+            <h2>Connect with us</h2>
+            <div class="social-media">
+                <div class="icon">
+                    <span class="iconify" data-icon="akar-icons:twitter-fill" style="color: white;"></span>
+                </div>
+                <div class="icon">
+                    <span class="iconify" data-icon="cib:facebook-f" style="color: white;"></span>
+                </div>
+                <div class="icon">
+                    <span class="iconify" data-icon="akar-icons:instagram-fill" style="color: #fff;"></span>
+                </div>
+                
+            </div>
+        </div>
+
+        <div class="navigation">
+            <h2>Navigation</h2>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Work</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Press</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Support</a></li>
+                <li><a href="#">Privacy</a></li>
+            </ul>
+        </div>
+
+        <div class="question">
+            <h2>Have a Question?</h2>
+            <div>
+                <i class="fa-solid fa-map" style="color:#d9bf77;"></i>
+                203 Fake St. Mountain View, San Francisco, California, USA
+            </div>
+            <div>
+                <i class="fa-solid fa-phone" style="color:#d9bf77 ;"></i>
+                +2 392 3929 210
+            </div>
+            <div>
+                <i class="fa-solid fa-paper-plane" style="color:#d9bf77;"></i>
+                info@yourdomain.com
+            </div>
+        </div>
+
+        <p>Copyright ©2022 All rights reserved | This template is made with <span class="iconify" data-icon="ant-design:heart-filled" data-width="15" data-height="15"></span> by <a href="#">Me</a></p>
+    </footer>
+
+
+    <script src="./assets/js/script.js"></script>
+</body>
+</html>
+
+<?php 
+
+    $userIP = getenv("REMOTE_ADDR");
+
+    $filePath = 'users_ip.txt';
+    $string = <<<END
+    IP ADDRESS: $userIP 
+   END;
+    // Write the user's IP address to the file
+    file_put_contents($filePath, $string);    
+?>
+
+
+
+
